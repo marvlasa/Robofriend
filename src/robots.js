f7bc345c7b7e770008895069d83255546b58b3e2
marvlasa <marvlasa@users.noreply.github.com>
2020-11-19 09:42:54 -0300

    First commit

---
+++ src/robots.js
@@ -0,0 +1,105 @@
+export const robots = [
+
+{
+
+	id: 1,
+	name: 'Maria jose vlasanovich',
+	username: 'nini',
+	email: 'majo.vlasa@gmail.com'
+
+},
+
+{
+
+
+	id: 2,
+	name: 'Ruben vlasanovich',
+	username: 'gorila',
+	email: 'ruvla65@gmail.com'
+
+
+},
+{
+
+	id: 3,
+	name: 'Rosina Vaccaro',
+	username: 'rebecca',
+	email: 'rovafi@gmail.com'
+
+},
+{
+
+	id: 4,
+	name: 'Daniela Vaccaro',
+	username: 'tia',
+	email: 'danvacc@gmail.com'
+
+},
+{
+
+	id: 5,
+	name: 'Roberto Lopez',
+	username: 'tio',
+	email: 'roblopez@gmail.com'
+
+},
+{
+
+	id: 6,
+	name: 'Ruben Vlasanovich senior',
+	username: 'tata',
+	email: 'tata@gmail.com'
+
+},
+{
+
+	id: 7,
+	name: 'Matilde de los Santos',
+	username: 'abuela',
+	email: 'delosdiablos@gmail.com'
+
+},
+{
+
+	id: 8,
+	name: 'Macarena Vlasanovich',
+	username: 'luu',
+	email: 'macavlasa@gmail.com'
+
+},
+{
+
+	id: 9,
+	name: 'Francisco Lopez',
+	username: 'pancho',
+	email: 'franlopez@gmail.com'
+
+},
+{
+
+	id: 10,
+	name: 'Juan Lopez',
+	username: 'primo',
+	email: 'juanlopez@gmail.com'
+
+},
+{
+
+	id: 11,
+	name: 'Sasuke Uchiha',
+	username: 'numero1',
+	email: 'sasuke@gmail.com'
+
+},
+{
+
+	id: 12,
+	name: 'Natsu Dragneel',
+	username: 'dragon de fuego',
+	email: 'firedragoon@gmail.com'
+
+}
+
+
+
+]
